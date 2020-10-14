824f51df1d89ec6da9e6d12845a8b02772783557
Eli Semory <elisemory@snap.com>
2020-10-14 13:54:41 -0400

    update HunterGate

--- cmake/HunterConfig.cmake
+++
@@ -1,3 +0,0 @@
-hunter_config(SPIRV-Headers
-              URL https://github.com/KhronosGroup/SPIRV-Headers/archive/1.5.3.reservations1.tar.gz
-              SHA1 e1666e3cf71e0eb8c75a1786e9f430f6d7670f42)
